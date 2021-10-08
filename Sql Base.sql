529b5aab04f4b1b4cf73399e8189c95013d20c81
RSLICING3D <79954920+RSLICING3D@users.noreply.github.com>
2021-10-08 09:38:14 -0500

    Add files via upload

---
+++ Sql Base.sql
@@ -0,0 +1,4 @@
+USE database_goku;
+INSERT INTO database_sensor (estado) VALUES (1);
+SELECT estado FROM database_buton;
+CREATE TABLE database_buton(id INT PRIMARY KEY AUTO_INCREMENT, estado INT, fecha char(20));
